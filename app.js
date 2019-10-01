e507b50c51c37a5f92625e9083ff5f7c0e4a3ea1
Nandini Nayak <nandini.r.nayak@gmail.com>
2019-10-01 16:57:52 +1000

    initial commit - routes, models, controllers and views created

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+// const methodOverride = require("method-override");
+
+const app = express();
+const port = 3000;
+
+mongoose.connect("mongodb://localhost/books_r_us", { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+
+mongoose.connection.on("error", err => console.log(err));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(require("./routes/author_routes"));
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
---
+++ controllers/author_controller.js
@@ -0,0 +1,26 @@
+const AuthorModel = require("../database/models/author_model");
+
+async function create(req, res) {
+    //logic for creating a resource
+    let { name, bio, gender } = req.body;
+    let author = await AuthorModel.create({ name, bio, gender })
+        .catch(err => res.status(500).send(err));
+    res.redirect("/authors");
+}
+
+async function index(req, res) {
+    //showed a list of all the resources
+    let authors = await AuthorModel.find();
+    res.render("author/index", { authors });
+}
+
+function make(req, res) {
+    //shows the form to create the resource
+    res.render("author/new");
+}
+
+module.exports = {
+    create,
+    index,
+    make
+}
